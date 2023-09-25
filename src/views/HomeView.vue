9aedaedd678bbdb99e50ece4f9a3083f8bb831da
Sergey Popov <sergey.po@danco.team>
2023-09-25 13:05:26 +0700

    change page title, add server host

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,141 @@
 <script setup lang="ts">
-
+import PersonPointer from '@/components/PersonPointer.vue'
 </script>
 
 <template>
-  <main>
-    
-  </main>
+  <div class="home-view-wrapper">
+    <header class="header">
+      <!-- <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav> -->
+      <p class="welcome">Welcome to</p>
+      <h1 class="title">Locate 360</h1>
+      <p class="caption">Find out where your loved ones are</p>
+    </header>
+    <main class="content">
+      <PersonPointer
+        class="person p1"
+        color="#7140FA"
+        photo-src="src/assets/images/main-person-1.jpeg"
+        size="50"
+      />
+      <PersonPointer
+        class="person p2"
+        color="#007AFF"
+        photo-src="src/assets/images/main-person-2.jpeg"
+        size="50"
+      />
+      <PersonPointer
+        class="person p3"
+        color="#FF8633"
+        photo-src="src/assets/images/main-person-3.jpeg"
+        size="56"
+      />
+      <PersonPointer
+        class="person p4"
+        color="#17B774"
+        photo-src="src/assets/images/main-person-4.jpeg"
+        size="53"
+      />
+      <PersonPointer
+        class="person p5"
+        color="#FF511B"
+        photo-src="src/assets/images/main-person-5.jpeg"
+        size="53"
+      />
+    </main>
+    <footer class="footer">
+      <button>Let’s Go</button>
+    </footer>
+  </div>
 </template>
+
+<style lang="scss">
+.home-view-wrapper {
+  display: flex;
+  flex-direction: column;
+  //if browser is not supported svh, height wil be 100vh
+  height: 100vh;
+  height: 100svh;
+
+  .header {
+    margin-top: 10.35%;
+    .welcome,
+    .title {
+      margin: 0;
+      font-size: 3.2rem;
+      line-height: 4rem;
+      font-weight: 600;
+    }
+    .title {
+      font-weight: 700;
+    }
+    .caption {
+      font-size: 1.4rem;
+      line-height: normal;
+      color: var(--color-text-blue);
+      margin: 1.63% 0 0;
+    }
+  }
+  .content {
+    margin: 3% 1.6rem 0;
+    flex-grow: 1;
+    position: relative;
+    background-image: linear-gradient(180deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0.1) 20%),
+      linear-gradient(0deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 20%),
+      url('../assets/images/main-bg.svg');
+    background-repeat: no-repeat;
+    background-position: top;
+    background-size: cover;
+    .person {
+      position: absolute;
+    }
+    .p1 {
+      top: 10%;
+      left: 10%;
+    }
+    .p2 {
+      top: 17%;
+      left: 80%;
+    }
+    .p3 {
+      top: 35%;
+      left: 35%;
+    }
+    .p4 {
+      top: 55%;
+      left: 15%;
+    }
+    .p5 {
+      top: 70%;
+      left: 63%;
+    }
+  }
+  .footer {
+    margin: 7.7% 0;
+    padding-bottom: 1rem;
+    align-self: center;
+    width: 100%;
+    max-width: 30rem;
+  }
+}
+
+// @media (min-width: 600px) {
+//   .home-view-wrapper {
+//     .content {
+//       margin: 3% 0 0;
+//     }
+//   }
+// }
+
+//Fix scroll in old Safari, when height: 100vh
+//https://qna.habr.com/q/953445
+@supports (-webkit-touch-callout: none) {
+  .home-view-wrapper {
+    height: -webkit-fill-available;
+  }
+}
+</style>
