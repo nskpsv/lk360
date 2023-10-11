7210eb5575ff4541334025b1db4d76d760e0b3be
Sergey Popov <nskpsv@mail.ru>
2023-10-11 14:50:09 +0700

    add favicon

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,5 @@
 <script setup lang="ts">
-import { onMounted } from 'vue';
 import { RouterView } from 'vue-router';
-import { useProgressStore } from './stores';
-import router from './router';
-
-// redirection if the application is not launched from the home view
-// onMounted(() => {
-//   const progress = useProgressStore();
-
-//   if (progress.current === 0) {
-//     router.push({ path: '/', replace: true });
-//   }
-// });
 </script>
 
 <template>
